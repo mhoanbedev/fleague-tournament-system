d556956fb843d3e80fa9f6d4b24cd58b7c5ff8be
hoanbedev <hoanvu2k5@gmail.com>
2025-12-16 12:38:08 +0700

    update final

--- routes/standings.routes.ts
+++ routes/standings.routes.ts
@@ -1,35 +1,30 @@
 import { Router } from "express";
 import * as controller from "../controllers/standings.controller";
 import * as authMiddleware from "../middleware/auth.middleware";
-import { autoUpdateLeagueStatus } from "../middleware/autoUpdateStatus.middleware";
 
 const router: Router = Router();
 
 router.get(
   "/league/:leagueId",
   authMiddleware.optionalAuth,
-  autoUpdateLeagueStatus,
   controller.getLeagueStandings
 );
 
 router.get(
   "/league/:leagueId/group/:group",
   authMiddleware.optionalAuth,
-  autoUpdateLeagueStatus,
   controller.getGroupStandings
 );
 
 router.get(
   "/league/:leagueId/all-groups",
   authMiddleware.optionalAuth,
-  autoUpdateLeagueStatus,
   controller.getAllGroupsStandings
 );
 
 router.get(
   "/league/:leagueId/stats",
   authMiddleware.optionalAuth,
-  autoUpdateLeagueStatus,
   controller.getLeagueStats
 );
 
--- routes/team.routes.ts
+++ routes/team.routes.ts
@@ -4,7 +4,6 @@ import * as authMiddleware from "../middleware/auth.middleware";
 import * as validate from "../middleware/team.validate.middleware";
 import { upload } from "../middleware/multer.middleware";
 import { uploadSingle } from "../helpers/uploadCloud";
-import { autoUpdateLeagueStatus } from "../middleware/autoUpdateStatus.middleware";
 
 const router: Router = Router();
 
@@ -12,14 +11,13 @@ const router: Router = Router();
 router.post(
   "/create",
   authMiddleware.authMiddleware,
-  autoUpdateLeagueStatus,
   upload.single("logo"),
   uploadSingle,
   validate.validateCreateTeam,
   controller.createTeam
 );
 
-router.get("/league/:leagueId",authMiddleware.optionalAuth,autoUpdateLeagueStatus, controller.getTeamsByLeague);
+router.get("/league/:leagueId",authMiddleware.optionalAuth, controller.getTeamsByLeague);
 
 router.get("/:id",authMiddleware.optionalAuth, controller.getTeamDetail);
 
@@ -37,7 +35,6 @@ router.delete("/:id", authMiddleware.authMiddleware, controller.deleteTeam);
 router.post(
   "/assign-groups/:leagueId",
   authMiddleware.authMiddleware,
-  autoUpdateLeagueStatus,
   controller.assignGroups
 );
 
