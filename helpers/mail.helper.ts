ab59e119b57e975c165fec39aa89608a06a5ba87
hoanbedev <hoanvu2k5@gmail.com>
2025-12-17 19:10:24 +0700

    update add send email replace nodeemailer wall sendgrif

--- helpers/mail.helper.ts
+++ helpers/mail.helper.ts
@@ -1,27 +1,50 @@
-import nodeMailer from 'nodemailer';
+// import nodeMailer from 'nodemailer';
 
-export const sendMail = async(
-    email: string, 
+// export const sendMail = async(
+//     email: string, 
+//     subject: string,
+//     html: string
+// ): Promise<void> => {
+//     try {
+//         const transporter = nodeMailer.createTransport({
+//             service: 'gmail',
+//             auth: {
+//                 user: process.env.EMAIL_USER,
+//                 pass: process.env.EMAIL_PASS
+//             }
+//         });
+//         const mailOptions = {
+//             from: process.env.EMAIL_USER,
+//             to: email,
+//             subject,
+//             html
+//         };
+//         await transporter.sendMail(mailOptions);
+//         console.log('Gửi email thành công đến:', email);
+//     } catch (error) {
+//         console.error('Gửi email thất bại:', error)
+//     }
+// }
+import dotenv from "dotenv";
+dotenv.config();
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+export const sendMail = async (
+    email: string,
     subject: string,
     html: string
-): Promise<void> => {
+) => {
     try {
-        const transporter = nodeMailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
+        await sgMail.send({
             to: email,
+            from: process.env.EMAIL_USER!,
             subject,
             html
-        };
-        await transporter.sendMail(mailOptions);
+        });
         console.log('Gửi email thành công đến:', email);
     } catch (error) {
-        console.error('Gửi email thất bại:', error)
+        console.error('Gửi email thất bại:', error);
     }
 }
